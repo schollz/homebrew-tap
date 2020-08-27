d14d8a0ef1baea11b33bfc4e1753af0e105c036b
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-26 19:20:30 -0700

    Brew formula update for croc version v8.1.0

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -2,21 +2,21 @@
 class Croc < Formula
   desc "croc is a tool that allows any two computers to simply and securely transfer files and folders."
   homepage "https://schollz.com/software/croc/"
-  version "8.0.13"
+  version "8.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/schollz/croc/releases/download/v8.0.13/croc_8.0.13_macOS-64bit.tar.gz"
-    sha256 "e79f1b6591ee9038bd70a884796c7413ecb3e8d44ffad0721e9167b0c9006b58"
+    url "https://github.com/schollz/croc/releases/download/v8.1.0/croc_8.1.0_macOS-64bit.tar.gz"
+    sha256 "a6b9193988ca74232ff686b5b6108ee56835b0a0a954f43594945b5be238aed0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/schollz/croc/releases/download/v8.0.13/croc_8.0.13_Linux-64bit.tar.gz"
-      sha256 "da9050aa62204e41a3de854b64bc9b8420a7b5c551ae2ce784c810751a0ca028"
+      url "https://github.com/schollz/croc/releases/download/v8.1.0/croc_8.1.0_Linux-64bit.tar.gz"
+      sha256 "965a3a8706dc97f0a8e83757859114763bcc2ee7c047b50c086ee1521e5852fa"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/schollz/croc/releases/download/v8.0.13/croc_8.0.13_Linux-ARM64.tar.gz"
-        sha256 "569b4e16e13cf650f8dd0f1aa6ba1c17ece6a1a632af534cd53e9e65b355d969"
+        url "https://github.com/schollz/croc/releases/download/v8.1.0/croc_8.1.0_Linux-ARM64.tar.gz"
+        sha256 "d0c0f7fbcd0c3c8675f59c59d9ec289d83789178b14938dfdde82c69d5aca1b9"
       else
       end
     end
