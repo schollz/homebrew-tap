66b3d173c73a4a0ce4fbacacdf3fa24e00673f9f
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-02 12:29:45 -0700

    Brew formula update for croc version v9.4.1

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -5,28 +5,28 @@
 class Croc < Formula
   desc "croc is a tool that allows any two computers to simply and securely transfer files and folders."
   homepage "https://schollz.com/software/croc/"
-  version "9.4.0"
+  version "9.4.1"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/schollz/croc/releases/download/v9.4.0/croc_9.4.0_macOS-64bit.tar.gz"
-      sha256 "8ea2b0af7573b3351c5befe522158d96c9fd5b779478465f06bf47d89f2a19e6"
+      url "https://github.com/schollz/croc/releases/download/v9.4.1/croc_9.4.1_macOS-64bit.tar.gz"
+      sha256 "3e7bb3cb5f6b4da0ed62757771b3dd4f24f5b7a5b9657526d86d51bff81f9cae"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/schollz/croc/releases/download/v9.4.0/croc_9.4.0_macOS-ARM64.tar.gz"
-      sha256 "75d4f840d8e4276e26a3a37dd38ca69b3b0e70d2bf94a99091757f872ea2cec5"
+      url "https://github.com/schollz/croc/releases/download/v9.4.1/croc_9.4.1_macOS-ARM64.tar.gz"
+      sha256 "0791c8004091b9a412c685e58bf81bfad679dac7532d438317e608ec78643872"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/schollz/croc/releases/download/v9.4.0/croc_9.4.0_Linux-64bit.tar.gz"
-      sha256 "e4e8e65ac6c99b8bf038ac9cc32d99c5d3b511e26ebafff7db7bd2fc01f01af0"
+      url "https://github.com/schollz/croc/releases/download/v9.4.1/croc_9.4.1_Linux-64bit.tar.gz"
+      sha256 "6e4f6bde87da591bb99b803eb6938f796a03b50008c9a6718d5f5ce2c13f7840"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/schollz/croc/releases/download/v9.4.0/croc_9.4.0_Linux-ARM64.tar.gz"
-      sha256 "225214577814e2babb839a8c1ed658ead9b184b36ac53642eaec234732f8705d"
+      url "https://github.com/schollz/croc/releases/download/v9.4.1/croc_9.4.1_Linux-ARM64.tar.gz"
+      sha256 "eea3732fd17ded8c48b1706cb665b2f2de53b8ed92de244b54616b053aa0539c"
     end
   end
 
